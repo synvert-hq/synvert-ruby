91348e839abaeb74987532814106d20826c38af4
Richard Huang <flyerhzm@gmail.com>
2024-02-18 18:52:01 +0800

    add --dont-respect-gitignore flag

--- lib/synvert/cli.rb
+++ lib/synvert/cli.rb
@@ -147,6 +147,10 @@ module Synvert
                   'loose mode, it ignores ruby version and gem version check, it uses strict mode by default' do |_loose|
             Core::Configuration.strict = false
           end
+          opts.on '--dont-respect-gitignore',
+                  'do not respect .gitignore, by default it ignores files and directories in .gitignore' do |_dont_respect_gitignore|
+            Core::Configuration.respect_gitignore = false
+          end
           opts.on '--test-result TEST_RESULT',
                   'result format of test, it can be actions or new_source, action is by default' do |test_result|
             Core::Configuration.test_result = test_result
